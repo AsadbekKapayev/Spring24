306a5084ae307e5bd0473584479ab55cfa200ca6
Kapayev Assabek <assadbekkapayev@gmail.com>
2024-05-18 22:55:09 +0600

    feat: scope added

--- src/main/java/org/example/annotation_based/service/UserService.java
+++ src/main/java/org/example/annotation_based/service/UserService.java
@@ -1,11 +1,14 @@
 package org.example.annotation_based.service;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Service("pool1")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class UserService {
 
     public UserService() {
